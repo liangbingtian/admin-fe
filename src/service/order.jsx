a93d617e71f23024cb7cc87b6916238745599d28
wld199 <649526182@qq.com>
2017-12-17 15:02:05 +0800

    Add files via upload

--- src/service/order.jsx
+++ src/service/order.jsx
@@ -14,37 +14,24 @@ const _mm = new MMUtil();
 export default class Order{
     // 获取订单列表
     getOrderList(listParam){
-        if(listParam.listType == 'list'){
+
             return _mm.request({
                 url     : _mm.getServerUrl('/manage/order/list.do'),
                 data    : {
-                    pageNum : listParam.pageNum || 1
+                    pageNum  : listParam.pageNum || 1,
+                    time_type: listParam.timetype|| 'day'
                 }
             });
         }
-        else if(listParam.listType == 'search'){
-            return _mm.request({
-                url     : _mm.getServerUrl('/manage/order/search.do'),
-                data    : listParam
-            });
-        } 
-    }
+
     // 获取订单详情
     getOrderDetail(orderNo){
         return _mm.request({
-            url     : _mm.getServerUrl('/manage/order/detail.do'),
+            url     : _mm.getServerUrl('/manage/order/search.do'),
             data    : {
                 orderNo : orderNo || 0
             }
         });
     }
-    // 发货
-    sendGoods(orderNo){
-        return _mm.request({
-            url     : _mm.getServerUrl('/manage/order/send_goods.do'),
-            data    : {
-                orderNo : orderNo || 0
-            }
-        });
-    }
+
 }
